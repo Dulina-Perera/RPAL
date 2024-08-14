23bfcacca758fb787c7b78888ab01e4805e91ada
Pyrokryos <dulina.perera@outlook.com>
2024-08-15 01:32:16 +0530

    Defined the `initialize` method.

--- Language-Server/server/src/server.ts
+++ Language-Server/server/src/server.ts
@@ -1,1 +1,16 @@
+import { initialize } from "./methods/initialize";
+import log from "./log";
 
+interface Message {
+	jsonrpc: string;
+}
+
+export interface RequestMessage extends Message {
+	id: number | string;
+	method: string;
+	params?: unknown[] | object;
+}
+
+process.stdin.on('data', (chunk) => {
+	log.write(chunk.toString());
+});
