a1b2c10b1532f2aa7fefb6e3082dbb777d52a478
Pyrokryos <dulina.perera@outlook.com>
2024-08-15 08:02:20 +0530

    Added the functionality to process a message when all its content is received.

--- Language-Server/server/src/server.ts
+++ Language-Server/server/src/server.ts
@@ -11,6 +11,28 @@ export interface RequestMessage extends Message {
 	params?: unknown[] | object;
 }
 
+let buffer: string = '';
 process.stdin.on('data', (chunk) => {
-	log.write(chunk.toString());
+	buffer += chunk;
+
+	while (true) {
+		const lengthMatch = buffer.match(/Content-Length: (\d+)\r\n/);
+		if (!lengthMatch) {
+			return;
+		} else {
+			const contentLength: number = parseInt(lengthMatch[1], 10);
+			const messageStart: number = buffer.indexOf('\r\n\r\n') + 4;
+
+			if (buffer.length < messageStart + contentLength) {
+				return;
+			} else {
+				const rawMessage: string = buffer.slice(messageStart, messageStart + contentLength);
+
+				const message: RequestMessage = JSON.parse(rawMessage);
+				log.write({ id: message.id, method:message.method });
+
+				buffer = buffer.slice(messageStart + contentLength);
+			}
+		}
+	}
 });
