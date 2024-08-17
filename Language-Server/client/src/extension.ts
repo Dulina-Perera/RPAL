b8b2f6b32af5c15e43dfc3db8a53b0e0a82b7e70
Dulina-Perera <dulina.perera@outlook.com>
2024-08-17 13:49:53 +0530

    Understood the purpose of the language server client.

--- Language-Server/client/src/extension.ts
+++ Language-Server/client/src/extension.ts
@@ -1,47 +1,51 @@
-import * as path from "path";
-import { workspace, ExtensionContext } from "vscode";
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
 
+import * as path from 'path';
+import { workspace, ExtensionContext } from 'vscode';
 import {
 	LanguageClient,
 	LanguageClientOptions,
 	ServerOptions,
-	TransportKind,
-} from "vscode-languageclient/node";
+	TransportKind
+} from 'vscode-languageclient/node';
 
 let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
 	// The server is implemented in node.
 	const serverModule: string = context.asAbsolutePath(
-		path.join("server", "out", "server.js")
+		path.join('server', 'out', 'server.js')
 	);
 
 	// If the extension is launched in debug mode then the debug server options are used.
 	// Otherwise the run options are used.
 	const serverOptions: ServerOptions = {
-		run: { module: serverModule, transport: TransportKind.stdio },
+		run: { module: serverModule, transport: TransportKind.ipc },
 		debug: { module: serverModule, transport: TransportKind.stdio }
 	};
 
 	// Options to control the language client
 	const clientOptions: LanguageClientOptions = {
-		// Register the server for all documents by default.
-		documentSelector: [{ scheme: "file", language: "*" }],
+		// Register the server for RPAL files.
+		documentSelector: [{ scheme: 'file', language: 'rpal' }],
 		synchronize: {
-			// Notify the server about file changes to `.clientrc` files contained in the workspace.
-			fileEvents: workspace.createFileSystemWatcher("**/.clientrc"),
-		},
+			// Notify the server about file changes to `.clientrc` files contained in the workspace
+			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
+		}
 	};
 
 	// Create the language client and start the client.
 	client = new LanguageClient(
-		"REPLACE_ME language-server-id",
-		"REPLACE_ME language server name",
+		'rpalLanguageClient',
+		'RPAL Language Client',
 		serverOptions,
 		clientOptions
 	);
 
-	// Start the client. This will also launch the server
+	// Start the client. This will also launch the server.
 	client.start();
 }
 
